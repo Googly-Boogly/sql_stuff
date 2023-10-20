f9d18afab546a8d545dbdc2ef91a2075a7ea5447
Googly-Boogly <mainalextaylor@gmail.com>
2023-10-19 23:30:37 -0500

    Update helpful_functions.py

--- helpful_functions.py
+++ helpful_functions.py
@@ -1,33 +1,79 @@
+# Sensitive info ln 100
+
 import logging
 import pymysql.cursors
 import os
+import traceback
+# from loguru import logger
 # This will be a universal page, for use across multiple projects
 
 
-def create_logger_error(file_name='temp', name='temp'):
-    # ---------------------------------------------------------------------------
-    # This will create a logger object that will display errors
-    # ---------------------------------------------------------------------------
+def log_it(logger, error):
+    """
+    Takes in logger and error and logs the error in the correct way
+    :param logger: logger object made in one of the logger maker functions
+    :param error: the error that occured
+    :return: None
+    """
+    log_message = f"An exception occurred on line {traceback.extract_tb(error.__traceback__)[-1].lineno}: {error}"
+    logger.error(log_message)
 
+def create_logger_error(file_path, name):
+
+    # ---------------------------------------------------------------------------
+    # This will create a logger object that will display everything
+    # ---------------------------------------------------------------------------
+    # to create logger: logger = create_logger_error(os.path.abspath(__file__), '')
+    """ To use the logger
+    from global_code.helpful_functions import create_logger_error, log_it
+    import traceback
+    logger = create_logger_error(os.path.abspath(__file__), '')
+    try:
+        x = 0/0
+    except Exception as e:
+        log_it(logger, e)
+    """
+
+    caller_dir = os.path.dirname(os.path.abspath(file_path))
+
+    # Create a 'logs' directory if it doesn't exist in the parent directory
+    logs_dir = os.path.join(caller_dir, 'logs')
+
+    os.makedirs(logs_dir, exist_ok=True)
+
+    # Create a subfolder named after the calling file (without extension)
+    calling_file_name = os.path.splitext(os.path.basename(file_path))[0]
+    file_logs_dir = os.path.join(logs_dir, calling_file_name)
+
+    os.makedirs(file_logs_dir, exist_ok=True)
+
+    # Define the log file path based on the provided name
+    log_file = os.path.join(file_logs_dir, f'{name}.log')
+
+    # Create the logger object
     logger = logging.getLogger(name)
 
-    logs_dir = "../utils/logs"
-    os.makedirs(logs_dir, exist_ok=True)  # Create the "logs" folder if it doesn't exist
-
-    log_dir = os.path.join(logs_dir, os.path.splitext(os.path.basename(str(file_name) + '.py'))[0])
-    os.makedirs(log_dir, exist_ok=True)  # Create the subfolder if it doesn't exist
-
-    log_file = os.path.join(log_dir, f"{name}.log")
+    # Create a file handler and set the formatter
     handler = logging.FileHandler(log_file)
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
 
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
+    # Set the logging level to capture all levels (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+    logger.setLevel(logging.DEBUG)
+
+    # Add the file handler to the logger
     logger.addHandler(handler)
+    logger.addHandler(console_handler)
+
     return logger
 
 
 # logging.basicConfig(level=logging.INFO, filename='log.log', filemode='w',
 #                         format="%(asctime)s - %(levelname)s - %(message)s")
+
 
 
 def create_logger_all(file_name, name):
@@ -78,6 +124,7 @@ class MySQLConnection:
             try:
                 query = cursor.mogrify(query, data)
                 # On off SQL Text buttton
+                print(query)
                 cursor.execute(query, data)
                 if query.lower().find("insert") >= 0:
                     # INSERT queries will return the ID NUMBER of the row inserted
@@ -92,6 +139,7 @@ class MySQLConnection:
                     self.connection.commit()
             except Exception as e:
                 # if the query fails the method will return FALSE
+                print(e)
                 return False
             finally:
                 # close the connection
@@ -104,7 +152,12 @@ def connecttomysql(db):
 
 
 def count_lines_of_code(directory):
-    # Enter in exact directory
+    """
+    Needs to be the absolute path of the directory also will exclude the venv folder
+    :param directory: (absolute path of the directory)
+    :return: total_lines of python in project
+    """
+
     total_lines = 0
 
     for root, dirs, files in os.walk(directory):
